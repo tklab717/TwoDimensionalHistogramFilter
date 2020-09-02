e55f60e9e7e553e4bf02f3cf29fa992e0029687c
Tomoya Kubo <tk.business126@gmail.com>
2020-09-02 22:44:16 +0900

    fix the code in the localizer.py for dealing with rectangle field.

--- localizer.py
+++ localizer.py
@@ -1,4 +1,4 @@
-# import pdb
+import pdb
 from helpers import normalize, blur
 
 def initialize_beliefs(grid):
@@ -26,25 +26,40 @@ def sense(color, grid, beliefs, p_hit, p_miss):
     
     return:
         new_beliefs:
-    
     """
     
     new_beliefs = []
-
+    total_probability =0
     #
     # TODO - implement this in part 2
     #
+    
+    for i, row in enumerate(grid):
+        belief_temp = []
+        for j, v in enumerate(row):
+            if color == v:
+                p = p_hit
+            else:
+                p = p_miss
+            belief_temp.append(p)
+            total_probability += p 
+        new_beliefs.append(belief_temp)
+    
+    for i,row in enumerate(new_beliefs):
+        for j,v in enumerate(row):
+            new_beliefs[i][j] = new_beliefs[i][j] / total_probability
 
     return new_beliefs
 
 def move(dy, dx, beliefs, blurring):
     height = len(beliefs)
     width = len(beliefs[0])
+    print(height)
     new_G = [[0.0 for i in range(width)] for j in range(height)]
     for i, row in enumerate(beliefs):
         for j, cell in enumerate(row):
-            new_i = (i + dy ) % width
-            new_j = (j + dx ) % height
+            new_i = (i + dy ) % height
+            new_j = (j + dx ) % width
             # pdb.set_trace()
             new_G[int(new_i)][int(new_j)] = cell
     return blur(new_G, blurring)
